669ec75bed8228e6a15e1c03f9e28f10c81205bf
Remy Jourde <remy@wannapix.com>
2016-09-21 09:02:46 +0200

    add english version

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/en", index_en)
 	http.HandleFunc("/resume", resume)
+	http.HandleFunc("/resume_en", resume_en)
 
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -24,7 +26,17 @@ func index(res http.ResponseWriter, req *http.Request) {
 	t.Execute(res, nil)
 }
 
+func index_en(res http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("templates/index_en.html")
+	t.Execute(res, nil)
+}
+
 func resume(res http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("templates/resume.html")
+	t.Execute(res, nil)
+}
+
+func resume_en(res http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("templates/resume_en.html")
 	t.Execute(res, nil)
 }
